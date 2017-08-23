1789f9f3847976575323d8a51d6894c70fd675d3
Jose Luis Esparza <sistemas6@atp.local>
2017-08-23 15:08:02 -0600

    new quotes and little fixes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 var json = [{quot:"Me empezaron a rechinar las nalgas",by:"El Victor L."}
 ,{quot:"Te veo, te escucho pero no te pongo atención",by:"El Paco"}
 ,{quot:"Tanto peca el que mata la vaca como el que es homosexual",by:"El Jack"}
-,{quot:"Infarto anal",by:"El Jesus"}
+,{quot:"Infarto anal",by:"El Chuy"}
 ,{quot:"Labios anales",by:"El Jack"}
 ,{quot:"Piensa que no estoy tonta",by:"La Estelita"}
 ,{quot:"Pinchi vaca recien muerta",by:"El Victor L."}
@@ -11,7 +11,7 @@ var json = [{quot:"Me empezaron a rechinar las nalgas",by:"El Victor L."}
 ,{quot:"El megazord de la homosexualidad",by:"El Victor L."}
 ,{quot:"El Mr. panty",by:"El Erick"}
 ,{quot:"Los faraones aztecas",by:"El Victor L."}
-,{quot:"Donde hay una hay mas",by:"El Jesus"}
+,{quot:"Donde hay una hay mas",by:"El Chuy"}
 ,{quot:"¿Un boiler solar necesita sol?",by:"El Albert"}
 ,{quot:"Fue actriz revelación a sus 2018",by:"El Armando"}
 ,{quot:"Son brasileñas japonesas",by:"El Mango"}
@@ -25,8 +25,8 @@ var json = [{quot:"Me empezaron a rechinar las nalgas",by:"El Victor L."}
 ,{quot:"Se puede usar para otros usos",by:"El Armando"}
 ,{quot:"Ella tiene mucho cambio a la resistencia",by:"La Estelita"}
 ,{quot:"Dejalo que se desplaye",by:"El Vic"}
-,{quot:"La conoexión de fibra de vidrio",by:"El Victor L."}
-,{quot:"Exhibicionador",by:"El Jesus"}
+,{quot:"La conexión de fibra de vidrio",by:"El Victor L."}
+,{quot:"Exhibicionador",by:"El Chuy"}
 ,{quot:" - ¿Como se dice muda en ingles? <br/> - Blind",by:"El Victor L."}
 ,{quot:"Yo estoy traumado . . . sigo traumado",by:"El mango"}
 ,{quot:"Eso lo iba a pagar cuando lo secara",by:"El Victor L."}
@@ -34,14 +34,14 @@ var json = [{quot:"Me empezaron a rechinar las nalgas",by:"El Victor L."}
 ,{quot:"Se me fue como agua (Estaba tomando agua)",by:"El Vic"}
 ,{quot:"Fornicador de penes",by:"El Victor L."}
 ,{quot:"Desvirginisarme",by:"El mango"}
-,{quot:"Extreñido",by:"El Jesus"}
+,{quot:"Extreñido",by:"El Chuy"}
 ,{quot:"Son mamones de esos que van de 2 weyes",by:"El Victor L."}
 ,{quot:"Mandame el ticket en un pantallazo",by:"El Jack"}
-,{quot:"Basflemos",by:"El Jesus"}
+,{quot:"Basflemos",by:"El Chuy"}
 ,{quot:"¿Como te llamas Estela?",by:"El Victor L."}
 ,{quot:"Vamos pa´ fuera a orearnos el ano",by:"El Victor L."}
 ,{quot:"50 billotes",by:"El Victor L."}
-,{quot:"Se mamaste",by:"EL Jesus"}
+,{quot:"Se mamaste",by:"El Chuy"}
 ,{quot:"Perra con acento en la P",by:"El Victor L."}
 ,{quot:"Esta mas rico mas carmela",by:"El Jack"}
 ,{quot:"Vas muy lejos pa'lla ",by:"El Vic"}
@@ -49,34 +49,34 @@ var json = [{quot:"Me empezaron a rechinar las nalgas",by:"El Victor L."}
 ,{quot:"Pon aquella pantalla negra a ver si jala",by:"El Victor L."}
 ,{quot:" - ¿Como se llaman a los hombres que nacen con vagina? <br/> - Mujeres ",by:"El Victor L."}
 ,{quot:"A ver cuantas gargantas mide",by:"El Victor L."}
-,{quot:"Huele como a pescadoso",by:"El Jesus"}
+,{quot:"Huele como a pescadoso",by:"El Chuy"}
 ,{quot:"Dura como 20 Gigas",by:"El Victor L."}
 ,{quot:"Son Gygaminutos",by:"El Victor L."}
 ,{quot:"Esa vieja chichuda yo la conozco",by:"El Victor L."}
 ,{quot:"La mas carnuda",by:"El Jack"}
 ,{quot:"A que hora te casaste",by:"El Victor L."}
-,{quot:"Esta preocupada con su computadora",by:"El Jesus"}
+,{quot:"Esta preocupada con su computadora",by:"El Chuy"}
 ,{quot:"Chocas con el aire",by:"El Victor L."}
-,{quot:"Si el sonido tuviera olor, ya oleria a mierda",by:"El Jesus"}
+,{quot:"Si el sonido tuviera olor, ya oleria a mierda",by:"El Chuy"}
 ,{quot:"Send nudes es lo mismo que pasate el pack pero en ingles",by:"El Victor L."}
 ,{quot:"Finales de Mayo principios de Agosto",by:"El Jack"}
 ,{quot:"Manda el Johnson",by:"El Mango"}
-,{quot:"Donde pone el ojo pone la cola",by:"El Jesus"}
-,{quot:"Sida en el parpado",by:"El Jesus"}
+,{quot:"Donde pone el ojo pone la cola",by:"El Chuy"}
+,{quot:"Sida en el parpado",by:"El Chuy"}
 ,{quot:"Sida del Maligno",by:"El Victor L."}
 ,{quot:"En la cama se pierden alturas",by:"El Victor L."}
-,{quot:"El Jesus - Dos cuerpos no pueen ocupar el mismo espacio <br/> El Vic - ¿Entonces quiere decir que si viajas al pasado no te puedes ver porque vales pa' pura verga?",by:""}
-,{quot:"Me estaban dando carrilla los de sistemas",by:"El Jesus (de sistemas)"}
+,{quot:"El Chuy - Dos cuerpos no pueden ocupar el mismo espacio <br/> El Vic - ¿Entonces quiere decir que si viajas al pasado no te puedes ver porque vales pa' pura verga?",by:""}
+,{quot:"Me estaban dando carrilla los de sistemas",by:"El Chuy (de sistemas)"}
 ,{quot:"La furia de santa rita",by:"El Jack"}
 ,{quot:"¿Ella es mexicana de México?",by:"El Armando"}
-,{quot:"¿Porque dice oficina si es un termo ?",by:"El Jesus"}
+,{quot:"¿Porque dice oficina si es un termo ?",by:"El Chuy"}
 ,{quot:"Palabras mas altas que idiota",by:"El Victor L."}
 ,{quot:"El episodio contraataca, la venganza del jedi",by:"El Jack"}
-,{quot:"Vamos a hacer un vuelga",by:"El Jesus"}
+,{quot:"Vamos a hacer un vuelga",by:"El Chuy"}
 ,{quot:"¿Te gusta mas Karla o mas Perla?",by:"El Vic"}
 ,{quot:"Ojala les de Eutanasia",by:"La Estelita"}
 ,{quot:"Esta bien lentium cuatro el internet",by:"El Vic"}
-,{quot:"Pinches codigos de los ´70s",by:"El Jesus"}
+,{quot:"Pinches codigos de los ´70s",by:"El Chuy"}
 ,{quot:"Yoladores del Yolo",by:"El Victor L."}
 ,{quot:"Bonaicezote,bonais.....¿Cómo se escribe?",by:"El Vic"}
 ,{quot:"Cuando estabas niño y guardabas el facebook en tu memoria",by:"El Victor L."}
@@ -86,19 +86,19 @@ var json = [{quot:"Me empezaron a rechinar las nalgas",by:"El Victor L."}
 ,{quot:"Ni que lo hubiera hecho internacionalmente",by:"El Victor L."}
 ,{quot:"Sexo fenenino",by:"El Victor L."}
 ,{quot:"La vagina de cuarentena",by:"El Victor L."}
-,{quot:"Que bueno que no andaste con ella",by:"El Jesus"}
+,{quot:"Que bueno que no andaste con ella",by:"El Chuy"}
 ,{quot:"Inflalible",by:"La Estelita"}
 ,{quot:"Porque nos difuminas asi",by:"El Jack"}
 ,{quot:"El vacio del infinito",by:"El Vic"}
 ,{quot:"El infinito no tiene numero",by:"El Victor L."}
-,{quot:"Solo recuerda que el 2 es mayor que 1",by:"El Jesus"}
+,{quot:"Solo recuerda que el 2 es mayor que 1",by:"El Victor L."}
 ,{quot:"No es cuestion de que seas negativo, eres antipositivo",by:"El Victor L."}
 ,{quot:"Coartana",by:"El Oscar"}
-,{quot:"Solo se van a quedar con mis reneguidos",by:"El Jesus"}
+,{quot:"Solo se van a quedar con mis reneguidos",by:"El Chuy"}
 ,{quot:"Lo extremizo",by:"El Armando"}
 ,{quot:"Impacto fecal",by:"El Jack"}
 ,{quot:"La base de todo es la base",by:"El Victor L."}
-,{quot:"No contestes, podria ser alguien",by:"El Jesus"}
+,{quot:"No contestes, podria ser alguien",by:"El Chuy"}
 ,{quot:"Se llama 'La diputada mas buena'",by:"El Armando"}
 ,{quot:"Me vas a matar a puro venenazo",by:"El Erick"}
 ,{quot:"No quiero escribir mal esa palabra que no existe",by:"El Vic"}
@@ -110,19 +110,19 @@ var json = [{quot:"Me empezaron a rechinar las nalgas",by:"El Victor L."}
     , {quot: "Se murio de cancer de sueño", by: "El Jack"}
     , {quot: "Esperaba lo que espero que ver", by: "El Paco"}
     , {quot: "Ponte la playera con la empresa", by: "El Victor L."}
-    , {quot: "No le dieron pa´ sus chicles", by: "El Jesus"}
+    , {quot: "No le dieron pa´ sus chicles", by: "El Chuy"}
     , {quot: "Cuando es renunciación", by: "El Jack"}
     , {quot: "Diva diminutivo de Divastacia", by: "El Victor L."}
     , {quot: "Por eso se llama Coca Cola porque es soda de coca", by: "El Paco"}
     , {quot: "Minusculas todas, hasta los numeros", by: "El Jack"}
-    , {quot: "Jack aunque este equivocado siempre tiene la verdad", by: "El Jesus"}
-   , {quot: "(Hablando de suicidios) Te cortas y vales winnie toda la vida", by: "El Jesus"}
+    , {quot: "Jack aunque este equivocado siempre tiene la verdad", by: "El Chuy"}
+   , {quot: "(Hablando de suicidios) Te cortas y vales winnie toda la vida", by: "El Chuy"}
     , {quot: "Pizza deep deep web", by: "El Paco"}
     , {quot: "Esa rolita hace mucho que no la veia", by: "El Paco"}
     , {quot: "La Lady Gaga cantaba mas chido que cuando ya no cantaba", by: "El Paco"}
     , {quot: "Cuando pienso me trabo", by: "El Paco"}
     , {quot: "Dicen que si no te manchas cuando comes es como si no hubieras comido", by: "El Paco"}
-    , {quot: "Terodaptilo", by: "El Jesus"}
+    , {quot: "Terodaptilo", by: "El Chuy"}
     , {quot: "Efecto Gaviota", by: "El Paco"}
     , {quot: "Ver un taco de ojo", by: "El Paco"}
     , {quot: "No zeto", by: "El Paco"}
@@ -138,7 +138,7 @@ var json = [{quot:"Me empezaron a rechinar las nalgas",by:"El Victor L."}
     , {quot: "Traje mi pantalon café porque lo voy a redescolorar", by: "El Paco"}
     , {quot: "Soy multiforma... ¿o como era? ... ¿Amorfo?", by: "El Paco"}
     , {quot: "Critolina", by: "El Victor L."}
-    , {quot: "Esa nube es de agua", by: "El Jesus"}
+    , {quot: "Esa nube es de agua", by: "El Chuy"}
     , {quot: "Invalido del pie", by: "El Victor L."}
     , {quot: "Ubreriano", by: "El Paco"}
     , {quot: "Pero si es fruta, porque es un refresco de cola", by: "El Paco"}
@@ -147,23 +147,23 @@ var json = [{quot:"Me empezaron a rechinar las nalgas",by:"El Victor L."}
     , {quot: "No me hagas emputar porque me emputo", by: "El Paco"}
     , {quot: "Golfo es una chingadera del agua", by: "El Armando"}
     , {quot: "Tengo unas amigas medio gordi-chidas", by: "El Paco"}
-    , {quot: "Obedece a la foca", by: "El Jesus"}
+    , {quot: "Obedece a la foca", by: "El Chuy"}
     , {quot: "El IMSS mata muertos", by: "El Paco"}
     , {quot: "Princesa Diana de España", by: "El Erick"}
     , {quot: "Pinchi tronote", by: "El Victor L."}
-    , {quot: "El lenwaffle", by: "El Jesus"}
+    , {quot: "El lenwaffle", by: "El Chuy"}
     , {quot: "Agarramos y queriamos", by: "El Vic"}
     , {quot: "Me voy hacer YouTube", by: "El Paco"}
     , {quot: "El mal del burrito de Avance", by: "El Paco"}
     , {quot: "Maquina cerebral", by: "El Victor L."}
-    , {quot: "Hister, no soy jister", by: "El Jesus"}
-    , {quot: "Abujerada", by: "El Jesus"}
+    , {quot: "Hister, no soy jister", by: "El Chuy"}
+    , {quot: "Abujerada", by: "El Chuy"}
     , {quot: "Botones alucinógenos ", by: "El Paco"}
     , {quot: "Hay que esperar que salga la lunula para convertirme en hombre lóbulo", by: "El Jack"}
     , {quot: "What do you fox say?", by: "El Victor L."}
     , {quot: "Azul alberca", by: "El Paco"}
     , {quot: "Pechugudo", by: "El Jack"}
-    , {quot: "Es como tener sangre en la sangre ¿no?", by: "El Jesus"}
+    , {quot: "Es como tener sangre en la sangre ¿no?", by: "El Chuy"}
     ,{quot:"Los swaggers esos que se cambian de pareja",by:"El Erik"}
     ,{quot:"Como dice mi amigo si la mierda esta hecha, usa la mierda",by:"El Paco"}
     ,{quot:"¿Tiene control paternal?",by:"La Estelita"}
@@ -172,13 +172,23 @@ var json = [{quot:"Me empezaron a rechinar las nalgas",by:"El Victor L."}
     ,{quot:"El hoyo fecal",by:"El Victor L"}
     ,{quot:"El desenrollo y el desenlace",by:"El Vic"}
     ,{quot:"Tiene un sensor de quitador de pila",by:"El Vic"}
-    ,{quot:"Se quedo en modo conservación",by:"El Jesus"}
-    ,{quot:"El ano dentado", by:"El Jesus"},
+    ,{quot:"Se quedo en modo conservación",by:"El Chuy"}
+    ,{quot:"El ano dentado", by:"El Chuy"},
     ,{quot:"Ortodoncista especialista en Ortos" , by:"El Vic"}
     ,{quot:"Sustituto de leche para polvo",by:"El Jack"}
     ,{quot:"Es que no saben el idioma Nigga",by:"El Vic"}
     ,{quot:"Cruza las manos,pa' que funcione",by:"El Paco"}
     ,{quot:"No tienen intolerancia",by:"El Erick"}
+    ,{quot:"Todo termino Abruscamente",by:"El Jack"}
+    ,{quot:"Esta lloviendo como cuando calló el Tsunami",by:"El Jack"}
+    ,{quot:"¿Porque cuando los perros andan en celo... Nosotros no nos calentamos ?",by:"El Armando"}
+    ,{quot:"Ya se venia viniendo",by:"El Victor L."}
+    ,{quot:"Un tanquecito de esos de leña",by:"El Chuy"}
+    ,{quot:"¿Y se mato adrede?, ¿O como se dice aldrede con d",by:"El Albert"}
+    ,{quot:"Frijoles meados",by:"El Maestro"}
+    ,{quot:"¿Como se abrevia tijuana? ¿TJ-uana?",by:"El Juanito"}
+    ,{quot:"¿Cuanto cuesta un DVD Blu-ray?",by:"El Albert"}
+    ,{quot:"¿Es tuyo o lo compraste?",by:"El Cesar"}
    ];
 
 function generateColor(){
